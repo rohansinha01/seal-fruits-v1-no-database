9f8523b5c5ba718629a13321d4afcd317216ca4c
rohansinha01 <rohansinha01@gmail.com>
2023-12-16 12:25:06 -0500

    new and create

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express()
 app.use(express.static("public")) // use a "public" folder for files
 // public/style.css -> /style.css
 // public/app.js -> /app.js
+// express.urlencoded (prase url encoded bodies)
+// add the data to req.body
+app.use(express.urlencoded({extended: true}))
 
 // fruits index route
 // get request to /fruits
@@ -22,6 +25,38 @@ app.get("/fruits", (req, res) => {
     // {fruits} => {fruits:fruits}
     res.render("index.ejs", {fruits})
 })
+
+// New Route - Render a page with a form
+// get request to /fruits/new
+// allow us to have a form to create a new fruit
+app.get("/fruits/new", (req, res) => {
+    // render a template with our form
+    // new.ejs = ./views/ + new.js
+    res.render("new.ejs")
+})
+
+// Create Route - Receives Form Data, Creates New Fruit
+// post request /fruits
+// create a fruit from the form data, then redirect back to index
+app.post("/fruits", (req, res) => {
+    // get the form data from the request
+    const body = req.body
+    // send back the form data as JSON
+    // res.send(body)
+    // convert the readyToEat to true/false
+    if (body.readyToEat === "on"){
+        body.readyToEat = true
+    } else {
+        body.readyToEat = false
+    }
+
+    // add the fruit to the array
+    fruits.push(body)
+
+    // redirect them back to index page
+    res.redirect("/fruits")
+})
+
 
 // fruits show route
 // get request to /fruits/:id
